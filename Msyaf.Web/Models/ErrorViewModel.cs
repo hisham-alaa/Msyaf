feed6a5eedc7c46b2a7d1d67af11346d17a44564
HishamAlaa <hishamalaa923@gmail.com>
2024-04-25 02:17:02 +0200

    Add project files.

---
+++ Msyaf.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Msyaf.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
